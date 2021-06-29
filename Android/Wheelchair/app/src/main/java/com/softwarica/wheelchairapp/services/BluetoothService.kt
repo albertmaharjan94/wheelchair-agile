71ee4200ec87f612c492797b79e4c843bd76e249
albert <albert.maharjan94@gmail.com>
2021-06-29 11:12:56 +0545

    service init

--- Android/Wheelchair/app/src/main/java/com/softwarica/wheelchairapp/services/BluetoothService.kt
+++ Android/Wheelchair/app/src/main/java/com/softwarica/wheelchairapp/services/BluetoothService.kt
@@ -1,6 +1,11 @@
 package com.softwarica.wheelchairapp.services
 
 import android.app.Service
+import android.content.Intent
+import android.os.IBinder
 
 class BluetoothService: Service {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
 }
